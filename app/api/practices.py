217ff3d0fda5642fa646b276c2d641eaaae059cf
riseryan89 <rise.ryan.lee@gmail.com>
2024-02-23 15:45:07 +0900

    Ch03/Clip02

--- app/api/practices.py
+++ app/api/practices.py
@@ -1,9 +1,38 @@
+import os
+
 from fastapi import APIRouter
+from enum import Enum
+
+
+class PythonFrameworkEnum(str, Enum):
+    DJANGO = "django"
+    FLASK = "flask"
+    FASTAPI = "fastapi"
 
 
 practice_router = APIRouter()
 
 
-@practice_router.get("/")
-async def root():
-    return {"message": "Hello World"}
+@practice_router.get("/sum")
+async def sum_calc(int1: int, int2: int):
+    return {"result": int1 + int2}
+
+
+@practice_router.get("/sum_str")
+async def sum_calc_w_str(int1: str, int2: str):
+    return {"result": int1 + int2}
+
+
+@practice_router.get("/frameworks/{framework}")
+async def get_frameworks(framework: PythonFrameworkEnum):
+    return {"frameworks": framework}
+
+
+@practice_router.get("/files/{file_path:path}")
+async def get_file(file_path: str):
+    os_path = os.path.join(os.getcwd(), "app/api", file_path)
+    if not os.path.exists(os_path):
+        return {"file_path": "File Not Found"}
+    with open(os_path, "r") as file:
+        content = file.read()
+    return content
